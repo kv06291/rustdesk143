768fa5378bdae24016c3b7e9e21e059f770c5d43
kv06291 <kv0629@126.com>
2025-11-14 11:21:26 +0800

    更改

--- src/lang/cn.rs
+++ src/lang/cn.rs
@@ -48,7 +48,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Privacy Statement", "隐私声明"),
         ("Mute", "静音"),
         ("Build Date", "构建日期"),
-        ("Version", "版本"),
+        ("Version", "版本:"),
         ("Home", "主页"),
         ("Audio Input", "音频输入"),
         ("Enhancements", "增强功能"),
